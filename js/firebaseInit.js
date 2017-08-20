b8ccdba8e7261bdcf8238f174f142faf885b588d
raidenx63 <raidenx63@gmail.com>
2017-08-20 20:07:45 +0800

    08/20/17::20:07
    -Added firebaseInit.js for singleton
    -Labelled what should be changed to install firebase

---
+++ js/firebaseInit.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyAD5LEi1srlvvmAMuBBZ5llYvuNjMaeq4A",
+	authDomain: "nxttech-e2ff2.firebaseapp.com",
+	databaseURL: "https://nxttech-e2ff2.firebaseio.com",
+	projectId: "nxttech-e2ff2",
+	storageBucket: "nxttech-e2ff2.appspot.com",
+	messagingSenderId: "591776999891"
+};
+
+firebase.initializeApp(config);
+
